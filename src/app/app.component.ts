6e1f6afa2588933082d81ffa7350f59052f0beef
Amdemichael Kidane <amkidane@excellerentsolutions.com>
2023-05-18 12:37:17 +0300

    Finalize cards app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      userName: 'nature',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'Saw this awesome tree during my hike today',
+    },
+    {
+      title: 'Neat Tree',
+      userName: 'mountainlover',
+      imageUrl: 'assets/mountain.jpeg',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      userName: 'biking12222',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'I did some biking today',
+    },
+  ];
 }
